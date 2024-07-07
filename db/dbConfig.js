b23d06ccc94cb27ea0e14a4332e7e70f2227194a
Mark Robertson <markrobertson67@gmail.com>
2024-07-07 16:48:49 -0400

    Added dbConfig code in new dev branch

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,3 +1,13 @@
 // Proprietary Software License
 // Copyright (c) 2024 Mark Robertson
 // See LICENSE.txt file for details.
+
+const pgp = require("pg-promise")();
+
+require('dotenv').config();
+
+const cn = process.env.DATABASE_URL
+
+const db = pgp(cn);
+
+module.exports = db;
