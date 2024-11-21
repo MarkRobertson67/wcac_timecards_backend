a505ef0b3229f28c00926e230e4395cd9c94f9fa
Mark Robertson <markrobertson67@gmail.com>
2024-11-20 21:12:51 -0500

    Added firebase_uid to employee table

--- db/schema.sql
+++ db/schema.sql
@@ -27,7 +27,8 @@ END $$;
 -- Create employees table
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
-    paychex_id VARCHAR(50) NOT NULL, -- PayChex ID for the employee
+    firebase_uid VARCHAR(100) UNIQUE,
+    paychex_id VARCHAR(50),
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -70,28 +71,5 @@ CREATE INDEX idx_work_date ON timecards(work_date);
 CREATE INDEX idx_employee_work_date ON timecards(employee_id, work_date);
 CREATE INDEX idx_status ON timecards(status);
 CREATE INDEX idx_is_admin ON employees(is_admin);
-
-
-
--- CREATE TABLE timecards (
---     id SERIAL PRIMARY KEY,
---     employee_id INT REFERENCES employees(id) ON DELETE CASCADE,
---     work_date DATE NOT NULL,
---     start_time TIME,
---     lunch_start TIME,
---     lunch_end TIME,
---     end_time TIME,
---     total_time INTERVAL,
---     status VARCHAR(20) DEFAULT 'active', -- ('active', 'submitted')
---     CONSTRAINT unique_employee_work_date UNIQUE (employee_id, work_date) -- Unique constraint
--- );
-
--- -- Create the enum type for role
--- DO $$ 
--- BEGIN
---     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'role_enum') THEN
---         CREATE TYPE role_enum AS ENUM ('Employee', 'Admin');
---     END IF;
--- END $$;
-
- -- status VARCHAR(20) DEFAULT 'active', -- ('active', 'submitted')
+CREATE INDEX idx_firebase_uid ON employees(firebase_uid);
+CREATE INDEX idx_email ON employees(email);
