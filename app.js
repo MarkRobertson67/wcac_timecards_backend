c89ee7d17f2a82c8597691016304aab80fd7cee5
Mark Robertson <markrobertson67@gmail.com>
2024-11-28 21:46:49 -0500

    Added CORS Options to fix CORS issues

--- app.js
+++ app.js
@@ -8,12 +8,21 @@ const cors = require("cors");
 const morgan = require('morgan');
 
 
+const corsOptions = {
+    origin: [
+        "https://wcac-timecards.netlify.app", // Production frontend
+        "http://localhost:3000" // Local development frontend
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+};
+
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('dev'));
 
